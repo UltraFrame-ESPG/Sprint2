d4fcdf453a918b22d74367829d754b942dfc843b
Isabella Yogui <isabellayk13@gmail.com>
2026-05-13 15:32:54 -0300

    Feat: Fazendo verificação de login e senha no JS

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+function logar(){
+
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+
+    if(email == "admin@gmail.com" && senha == "123"){
+
+        mostrarMensagem("Acesso liberado!");
+
+        setTimeout(() => {
+            window.location.href = "./src/pages/home.html";
+        }, 2000);
+
+    }else{
+
+        mostrarMensagem("Email ou senha incorretos!");
+
+    }
+
+}
+function mostrarMensagem(texto){
+
+    let msg = document.getElementById("mensagem");
+
+    msg.innerHTML = texto;
+    msg.style.opacity = "1";
+
+    setTimeout(() => {
+        msg.style.opacity = "0";
+    }, 3000);
+
+}
